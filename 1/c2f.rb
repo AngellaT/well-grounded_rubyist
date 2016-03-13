9adfea86219ed8c75770942f8cf295b16394072c
jim thompson <j@jimthompson.cc>
2016-03-13 01:00:52 +0000

    Take input from the user

--- 1/c2f.rb
+++ 1/c2f.rb
@@ -1,10 +1,18 @@
 #!/home/jim/.rvm/rubies/ruby-1.9.1-p431/bin/ruby
 
-celsius = 100
+puts "Enter a temperature in celsius: "
+celsius = gets.chomp
+if celsius.to_f % 1 > 0
+    celsius = celsius.to_f
+else
+    celsius = celsius.to_i
+end
 
 
 def convert_to_celsius(number)
     (number * 9 / 5) + 32
 end
+
+fahrenheit = convert_to_celsius(celsius)
 
 puts "The result is: #{fahrenheit}."

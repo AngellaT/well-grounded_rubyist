3b94df957fd570a1dc59f08afefa0a2954546d79
jim thompson <j@jimthompson.cc>
2016-03-13 00:58:01 +0000

    stick the coversion in a method

--- 1/c2f.rb
+++ 1/c2f.rb
@@ -1,5 +1,10 @@
 #!/home/jim/.rvm/rubies/ruby-1.9.1-p431/bin/ruby
 
 celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
+
+
+def convert_to_celsius(number)
+    (number * 9 / 5) + 32
+end
+
 puts "The result is: #{fahrenheit}."

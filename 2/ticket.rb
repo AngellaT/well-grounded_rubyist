cb6bb4348dad40fbb77eebe96e5b7aa185751988
jim thompson <j@jimthompson.cc>
2016-03-15 09:44:24 +0000

    Examples -- Intro to objects

---
+++ 2/ticket.rb
@@ -0,0 +1,31 @@
+#!/home/jim/.rvm/rubies/ruby-1.9.1-p431/bin/ruby
+
+ticket = Object.new
+
+def ticket.venue
+  "Town Hall"
+end
+
+def ticket.performer
+  "Mark Twain"
+end
+
+def ticket.event
+  "Author's reading"
+end
+
+def ticket.price
+  5.50
+end
+
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+
+def ticket.date
+  "01/02/03"
+end
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}, on #{ticket.date}."
+puts "The performer is #{ticket.performer}."
+puts "The seat is #{ticket.seat} and it costs $%.2f." % ticket.price
